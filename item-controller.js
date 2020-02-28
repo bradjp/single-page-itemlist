bc664f80f6187e5ebdc50bdc0bb0fc801ab8f39e
Brad Phillips <bjrp@protonmail.com>
2020-02-28 12:59:04 +0000

    Items show on new page

--- item-controller.js
+++ item-controller.js
@@ -10,5 +10,38 @@
     element.innerHTML = html
   }
 
+  function makeUrlChangeShowItemForCurrentPage() {
+    window.addEventListener("hashchange", showItemForCurrentPage);
+  };
+
+  function showItemForCurrentPage() {
+    showItem(getItemFromUrl(window.location));
+  };
+
+  function getItemFromUrl(location) {
+    x = location.hash.split("#")[1];
+    return x = x.split('/')[1]
+  };
+
+  function showItem(Item) {
+    x = search(Number(Item), this.controller.itemList.items)
+    document
+      .getElementById("app")
+      .innerHTML = x.text
+      
+  };
+
+  function search(IDKey, arr){
+    for (var i=0; i < arr.length; i++) {
+        if (arr[i].id === IDKey) {
+            return arr[i];
+        }
+    }
+}
+
+  makeUrlChangeShowItemForCurrentPage()
+
   exports.ItemController = ItemController
+  exports.makeUrlChangeShowItemForCurrentPage = makeUrlChangeShowItemForCurrentPage
+  exports.search = search
 })(this)
